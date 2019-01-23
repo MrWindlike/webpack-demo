de22c0ef64a0d7ad23762f4c7f815b9618a59988
windlike <838596151@qq.com>
2019-01-24 00:24:07 +0800

    feat: add babel-plugin-transform-react-remove-prop-types

--- .babelrc.js
+++ .babelrc.js
@@ -12,12 +12,12 @@ module.exports = {
         "@babel/plugin-transform-classes",
         "@babel/plugin-transform-arrow-functions",
         "@babel/plugin-transform-template-literals",
-        "@babel/plugin-syntax-dynamic-import"
+        "@babel/plugin-syntax-dynamic-import",
     ],
     env: {
         production: {
             plugins: [
-                "transform-react-remove-prop-types"
+                "transform-react-remove-prop-types",
             ]
         }
     }
